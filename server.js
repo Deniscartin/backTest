560114462e9148355bae2c1ff3b29b0138c9a52a
Deniscartin <100749943+Deniscartin@users.noreply.github.com>
2024-08-31 18:05:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 
 const corsOptions = {
-  origin: 'https://fronttest-eazrwtdu.daucu.site/', // Your frontend origin
+  origin: 'https://fronttest-eazrwtdu.daucu.site', // Your frontend origin
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type'],
 };
@@ -12,7 +12,7 @@ app.use(cors(corsOptions));
 
 app.get('/api/customers', (req, res) => {
   const customers = [
-    { id: 1, firstName: 'Pietro', lastName: 'Doe' },
+    { id: 1, firstName: 'Pino', lastName: 'Doe' },
     { id: 2, firstName: 'anna', lastName: 'Traversy' },
     { id: 3, firstName: 'Giovanni', lastName: 'Swanson' },
   ];
